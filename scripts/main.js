37cb0e66c881f8cea1fe8685eead10b8eafd5302
frrf <franciscopali71@gmail.com>
2021-01-26 14:39:27 -0800

    Can now populate checkboxes for all habits, now need to do two things, the first is to not allow another date selection to append more boxes to existing--instead clear old add new, second is to add the functionality to add a habit (so delete the habit example ones), later I will add labels to each checkbox (above, most likely), to indicate what day of the month it is

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,40 @@
+let days = (month,year) => {
+    // return the number of days in a month, given user input parameters
+    return new Date(year, month, 0).getDate();
+ };
+
+let checkboxElements = document.getElementsByClassName("row__checkboxes");
+
 document.querySelector('input[type="date"]').addEventListener("change", function() {
     let input = this.value;
     let year = input.slice(0,4)
     let month = input.slice(5,7)
-
+    let daysInMonth = parseInt(days(month, year));
     //call function to add days(month, year) # of checkboxes
-
-    console.log("Days in " + month + ":" +days(month, year));
+    addTotalDaysCheckboxes(daysInMonth, checkboxElements)
+    console.log("Days: " + daysInMonth);
 })
-//-------------------------------------------------------------------------------------
 
-let days = function(month,year) {
-    return new Date(year, month, 0).getDate();
- };
+// adds a checkbox object to to div row__checkboxes for ever day in user selected month
+let addTotalDaysCheckboxes = (days, checkboxesDiv) => {
+    // loops through # of habits, j = div
+    for (let j = 0; j < checkboxesDiv.length; j++) {
+        // loops through # of days, i = day #
+        for(let i = 0; i < days; i++) {
+            // populates habit with checkbox
+            checkboxesDiv[j].append(createCheckbox());
+            console.log("Hello");
+        }
+    }
+}
+
+// returns a checkbox object when called
+let createCheckbox = () => {
+    // creating checkbox element 
+    let checkbox = document.createElement('input');     
+    // Assigning the attributes to created checkbox 
+    checkbox.type = "checkbox"; 
+    checkbox.name = ""; 
+    checkbox.id = ""; 
+    return checkbox;
+}
