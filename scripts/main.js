692fa0a73f39f1e167b264c7b699ee620413a3bc
frrf <franciscopali71@gmail.com>
2021-01-27 17:10:28 -0800

    the checkboxes are generated upon user inputing habit then date

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,44 @@
+// contains text input element with user's habit
+let habit = document.getElementById('habitTextInput');
+// contains the button that adds the habit to habitTrackerArea
+const habitInputButton = document.getElementById('add_habit_btn');
+// contains the div that is parent to each habit
+const habitTrackerArea = document.getElementById('trackerArea');
+
+
+habitInputButton.addEventListener('click', () => {
+    if (habit.value !== "") {
+        addHabit();
+    }
+});
+habit.addEventListener("keydown", event => {
+    if (habit.value !== "" && event.key === "Enter") {
+        habitInputButton.click();
+    }
+});
+
+function addHabit() {
+    //habit.value should be a <div class="habit__row"> object
+    habitTrackerArea.innerHTML += 
+    `<div class='habit__row'>
+        <label for='' class='row__label'>${habit.value}:</label>
+        <div class='row__checkboxes'>
+        </div>
+    </div>`;
+    habit.value = "";
+  }
+
+// holds the divs that contain the checkboxes for each habit 
+let checkboxElements = document.getElementsByClassName("row__checkboxes");
+
+// return the number of days in a month, given user input parameters
 let days = (month,year) => {
-    // return the number of days in a month, given user input parameters
     return new Date(year, month, 0).getDate();
  };
 
-let checkboxElements = document.getElementsByClassName("row__checkboxes");
-
-document.querySelector('input[type="date"]').addEventListener("change", function() {
-    let input = this.value; // "XXXX-XX-XX" Year, Month, Day
-    let year = input.slice(0,4)
-    let month = input.slice(5,7)
-    let daysInMonth = parseInt(days(month, year));
-    
-    //call function to add days(month, year) # of checkboxes
-    addTotalDaysCheckboxes(daysInMonth, checkboxElements)
-    console.log("Days: " + daysInMonth);
-})
-
 // adds a checkbox object to to div row__checkboxes for ever day in user selected month
 // calls the deleteChild function to ensure div is empty before appending checkboxes
-let addTotalDaysCheckboxes = (days, checkboxesDiv) => {
+let addTotalDaysCheckboxes = (days, checkboxesDiv, day) => {
     // loops through # of habits (parent element), j = div
     for (let j = 0; j < checkboxesDiv.length; j++) {
         // deletes present checkboxes of the div
@@ -26,20 +46,31 @@ let addTotalDaysCheckboxes = (days, checkboxesDiv) => {
         // loops through # of days, i = day #
         for(let i = 0; i < days; i++) {
             // populates habit with checkbox
-            checkboxesDiv[j].append(createCheckbox());
+            checkboxesDiv[j].append(createCheckbox(i+1, day));
         }
     }
 }
 
 // returns a checkbox object when called
-let createCheckbox = () => {
+let createCheckbox = (num, day) => {
+    // creating label element 
+    let label = document.createElement('label');     
+    label.for = `check-${num}`;
+    label.innerText = `Day: ${num}`;
     // creating checkbox element 
     let checkbox = document.createElement('input');     
     // Assigning the attributes to created checkbox 
     checkbox.type = "checkbox"; 
-    checkbox.name = ""; 
-    checkbox.id = ""; 
-    return checkbox;
+    checkbox.id = `check-${num}`;
+    if (num < day) {
+        checkbox.disabled = true;
+        label.style= "display:none;"
+    }
+
+
+    label.append(checkbox);
+
+    return label;
 }
 
 // deletes all the children in parent element (used for checkboxes)
@@ -50,3 +81,16 @@ let deleteChild = (parent) => {
         child = parent.lastElementChild; 
     } 
 } 
+
+document.querySelector('input[type="date"]').addEventListener("change", function() {
+    let input = this.value; // "XXXX-XX-XX" Year, Month, Day
+    let year = input.slice(0,4)
+    let month = input.slice(5,7)
+    let day = input.slice(8,10);
+    let daysInMonth = parseInt(days(month, year));
+    
+    //call function to add days(month, year) # of checkboxes
+    addTotalDaysCheckboxes(daysInMonth, checkboxElements, day);
+    console.log("Days: " + daysInMonth);
+    console.log("Day: " + day);
+})
