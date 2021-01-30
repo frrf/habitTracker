91f8509050ca95533969a6ecca08511d3ca77a1a
frrf <franciscopali71@gmail.com>
2021-01-30 13:17:35 -0800

    cleaned up the calendarInput function that is called when user selcts date in input element

--- scripts/main.js
+++ scripts/main.js
@@ -36,52 +36,62 @@ habit.addEventListener("keydown", event => {
     }
 });
 
-document.querySelector('input[type="date"]').addEventListener("change", function() {
-    let input = this.value; // "XXXX-XX-XX" Year, Month, Day
-    let year = input.slice(0,4)
-    let month = input.slice(5,7)
-    let day = input.slice(8,10);
-    let daysInMonth = parseInt(days(month, year));
-    
-    //call function to add days(month, year) # of checkboxes
-    addTotalDaysCheckboxes(daysInMonth, checkboxElements, day, year, month);
-    console.log("Days: " + daysInMonth);
-    console.log("Day: " + day);
+document.querySelector('input[type="date"]').addEventListener("change", calendarInput);
 
-
-
-    // store new div to local storage -------------------------------------------------
-    let userStorage = window.localStorage;
-    userStorage.setItem('test', trackerArea.innerHTML);
-})
-
-//#############################################################################################\\
+//#######################################################################################\\
 
 
 
 //#################################    Functions    #################################\\
-// return the number of days in a month, given user input parameters
-let days = (month,year) => {
-    return new Date(year, month, 0).getDate();
- };
 
+function calendarInput() {
+    let input = this.value; // "XXXX-XX-XX" Year, Month, Day
+    let year = parseInt(input.slice(0,4));
+    let month = parseInt(input.slice(5,7));
+    let day = parseInt(input.slice(8,10));
+    let daysInMonth = parseInt(days(month, year));
+    
+    let dateInfo = {"dayChosen": day,
+                    "days": daysInMonth,
+                    "month": month,
+                    "year": year 
+                };
+    //call function to add # of checkboxes
+    addTotalDaysCheckboxes(dateInfo);
+    //call function to update local storage
+    store();
+}
 // adds a checkbox object to to div row__checkboxes for ever day in user selected month
 // calls the deleteChild function to ensure div is empty before appending checkboxes
-let addTotalDaysCheckboxes = (days, checkboxesDiv, day, year, month) => {
+let addTotalDaysCheckboxes = (dateInfoObject) => {
+// let addTotalDaysCheckboxes = (days, day, year, month) => {
     // loops through # of habits (parent element), j = div
-    for (let j = 0; j < checkboxesDiv.length; j++) {
+    for (let j = 0; j < checkboxElements.length; j++) {
         // deletes present checkboxes of the div
-        deleteChild(checkboxesDiv[j]);
+        deleteChild(checkboxElements[j]);
         // loops through # of days, i = day #
-        for(let i = 0; i < days; i++) {
+        for(let i = 0; i < dateInfoObject.days; i++) {
             // populates habit with checkbox
-            let checkbox = createCheckbox(i+1, day, year, month);
+            let checkbox = createCheckbox(i+1, dateInfoObject.dayChosen, dateInfoObject.year, dateInfoObject.month);
             if (typeof checkbox === 'object' && checkbox !== null) {
-                checkboxesDiv[j].append(checkbox);
+                checkboxElements[j].append(checkbox);
             }
         }
     }
 }
+
+
+// store trackerArea div to local storage
+function store() {
+    let userStorage = window.localStorage;
+    userStorage.setItem('test', trackerArea.innerHTML);
+}
+
+// return the number of days in a month, given user input parameters
+let days = (month,year) => {
+return new Date(year, month, 0).getDate();
+};
+
 
 // returns a checkbox object when called
 let createCheckbox = (num, day, year, month) => {
@@ -124,19 +134,20 @@ let deleteChild = (parent) => {
     } 
 } 
 
-function createHabit(habit) {
+function createHabit(newHabit) {
+    let habitText = newHabit.value;
     let divHabitRow = document.createElement('div');
     divHabitRow.classList.add("habit__row");
 
     let rowLabelDiv = document.createElement('label');
     rowLabelDiv.classList.add("row__label");
-    rowLabelDiv.textContent = habit.value;
+    rowLabelDiv.textContent = habitText;
 
     let rowCheckboxesDiv = document.createElement('div');
     rowCheckboxesDiv.classList.add("row__checkboxes");
 
     divHabitRow.append(rowLabelDiv);
     divHabitRow.append(rowCheckboxesDiv);
-    habit.value = "";
+    newHabit.value = "";
     return divHabitRow;
 }
